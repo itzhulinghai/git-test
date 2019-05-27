af544303305868e245d9bdadb89006a326e57cfe
itzhulinghai <127454789@qq.com>
2019-05-27 18:10:08 +0800

    第一次提交

---
+++ git-test-project/src/com/yxb/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.yxb.git;
+
+/**
+ * @Description:
+ * @Author: zhulh
+ * @CreateDate: 2019/5/27 0027$ 18:00$
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+
+
+}
